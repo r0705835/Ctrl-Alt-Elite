17ff489c63592c5d6cc91c874f3ebf223b2821c6
RaymondGericke <576839@student.belgiumcampus.ac.az>
2020-05-16 01:23:32 +0200

    Update

--- ClassLibrary1/Model/Application.cs
+++ ClassLibrary1/Model/Application.cs
@@ -2,50 +2,6 @@
 
 namespace Model
 {
-    class Users
-    {
-        public int UserID { get; set; }
-        public string UserName { get; set; }
-    }
-
-    [Serializable]
-    class InvalidUserNameException : Exception
-    {
-        public InvalidUserNameException()
-        {
-
-        }
-
-        public InvalidUserNameException(string name)
-            : base(String.Format("Invalid User Name: {0}", name))
-        {
-
-        }
-
-    }
-
-    class Products
-    {
-        public int ProduceID { get; set; }
-        public string ProductName { get; set; }
-    }
-
-    [Serializable]
-    class InvalidProductNameException : Exception
-    {
-        public InvalidProductNameException()
-        {
-
-        }
-
-        public InvalidProductNameException(string name)
-            : base(String.Format("Invalid Product Name: {0}", name))
-        {
-
-        }
-
-    }
-
     public delegate void MyDelegate(string msg);
 
    
