ca2872aabcd1a6b9f5829c35ff1130b5c914e496
Wannes <Wannes.f@gmail.com>
2020-05-16 03:20:22 +0200

    CHanges to reward, product and user

--- ClassLibrary1/Model/User.cs
+++ ClassLibrary1/Model/User.cs
@@ -4,10 +4,50 @@ using System.Text;
 
 namespace Model
 {
-       class Users
+       class User
     {
         public int UserID { get; set; }
         public string UserName { get; set; }
+
+        public int ecoscore { get; set; }
+
+
+
+        public User(int userid, string username) {
+            this.UserID = userid;
+            this.UserName = username;
+            this.ecoscore = 0;
+        }
+
+
+
+        public int getEcoScore() { return this.ecoscore; }
+
+        public string getUserNamee() { return this.UserName; }
+
+        public int getUserID() { return this.UserID; }
+
+
+
+        public void buy(Product product)
+        {
+            if (product == null) { throw new System.ArgumentException("Product is null"); }
+            this.ecoscore += product.getRating();
+
+        }
+
+
+
+        public void redeem(Reward reward) {
+            if (reward == null) { throw new System.ArgumentException("reward is null"); }
+            this.ecoscore -= reward.getPrice();
+        }
+
+
+
+
+
+
     }
 
     [Serializable]
