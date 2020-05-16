58fc2005c1f52dcfc11d238140a4b2a84f02f0d4
Tanielle <taniellepettitt@gmail.com>
2020-05-16 09:12:06 +0200

    Final Teamviewer

--- ClassLibrary1/MainForm/MainWindow.xaml.cs
+++ ClassLibrary1/MainForm/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MainForm;
+
 
 namespace View
 {
@@ -23,6 +25,11 @@ namespace View
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
